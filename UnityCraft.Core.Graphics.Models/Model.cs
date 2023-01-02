2b8381c8956c2a95a8375b274184148e9f6a88e4
SixtyHrtz <m.sitnik.by@gmail.com>
2023-01-02 23:59:51 +0300

    Added "Models" project

---
+++ UnityCraft.Core.Graphics.Models/Model.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace UnityCraft.Core.Graphics.Models
+{
+    public class Model
+    {
+        private Stream stream;
+
+        public Model(Stream stream)
+        {
+            if (stream == null)
+            {
+                throw new ArgumentNullException(nameof(stream));
+            }
+
+            this.stream = stream;
+
+            using (var reader = new BinaryReader(stream, Encoding.ASCII, true))
+            {
+
+            }
+        }
+    }
+}
